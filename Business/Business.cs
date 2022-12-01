d0633bb7ab84d6fd0d1cd674fba7cd601dc2f5ea
Jake Nguyen <JakeNg0424@gmail.com>
2022-11-30 20:11:20 -0500

    Implement Logic

--- Business/Business.cs
+++ Business/Business.cs
@@ -11,8 +11,34 @@ public class Business : IBusiness
         ProductsRepository = repo;
     }
 
-    List<string> IBusiness.GetTopThreeProductsWith(int top, int starsNeeded)
+    public List<string> GetTopThreeProductsWith(int top, int starsNeeded)
     {
-        throw new NotImplementedException();
+        var allProducts = ProductsRepository.Get();
+
+        Dictionary<(int, string), int> frequency = new Dictionary<(int, string), int>();
+        foreach (var product in allProducts.Products)
+        {
+            var countReview = product.Reviews.Where(r => r.Star == starsNeeded).Count();
+            frequency.Add((product.Id, product.Name), countReview);
+        }
+
+        PriorityQueue<(int, string), int> queue = new PriorityQueue<(int, string), int>();
+        foreach (var item in frequency)
+        {
+            queue.Enqueue(item.Key, item.Value);
+            if (queue.Count > top)
+            {
+                queue.Dequeue();
+            }
+        }
+
+        List<string> productNames = new List<string>();
+        while (queue.Count > 0)
+        {
+            var productTuple = queue.Dequeue();
+            productNames.Add(productTuple.Item2);
+        }
+
+        return productNames;
     }
 }
